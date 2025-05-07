3cdff844c2733a95dd832a7d80cd8d0bcee5f94a
D-Seagull <136006992+Seagull-D@users.noreply.github.com>
2025-05-07 22:38:14 +0200

    added izi toast

--- src/js/sendMail.js
+++ src/js/sendMail.js
@@ -1,8 +1,36 @@
-export const sendMail = ({ name, email, message }) => {
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+export const sendMail = async ({ name, email, message }) => {
   let params = {
     name,
     email,
     message,
   };
-  emailjs.send('service_zn3aqae', 'template_3o02fkr', params);
+  await emailjs
+    .send('service_zn3aqae', 'template_3o02fkr', params)
+    .then(() =>
+      iziToast.success({
+        title: 'Wysłane!',
+        message: 'Odpowiemy jak najszybciej',
+        position: 'center',
+        theme: 'light',
+        color: '#7DE2D1',
+        messageColor: '#1B1B1B',
+        titleColor: '#1B1B1B',
+        progressBar: false,
+        timeout: 4000,
+        layout: 2,
+        transitionIn: 'fadeInDown',
+        transitionOut: 'fadeOutUp',
+      })
+    )
+    .catch(error =>
+      iziToast.error({
+        title: 'Wystapil blad!',
+        message: 'Sprobuj pozdniej ',
+        position: 'center',
+        color: 'red',
+        layout: 2,
+      })
+    );
 };
