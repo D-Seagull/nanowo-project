5c6a6f8797a7c7e12b07e40d560588bb6ac4e34c
D-Seagull <136006992+Seagull-D@users.noreply.github.com>
2025-05-10 11:49:58 +0200

    optim mob menu

--- src/js/headerMenu.js
+++ src/js/headerMenu.js
@@ -26,15 +26,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
 /*-------------home-link------------*/
 const homeModalLink = document.querySelector('#home-page-link-modal');
-if (window.location.pathname === '/index.html') {
-  homeModalLink.style.display = 'none';
-}
 const aboutModalLink = document.querySelector('#about-page-link-modal');
 const implModalLink = document.querySelector('#impl-page-link-modal');
 const updatesModalLink = document.querySelector('#updates-page-link-modal');
 const contactModalLink = document.querySelector('#contact-page-link-modal');
 
 switch (window.location.pathname) {
+  case '/index.html':
+    homeModalLink.style.display = 'none';
+    break;
   case '/about.html':
     aboutModalLink.classList.add('active-page');
     break;
