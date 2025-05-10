083c9d526608ab3d51904e8de96de859069d74ff
D-Seagull <136006992+Seagull-D@users.noreply.github.com>
2025-05-10 23:31:15 +0200

    added active page for tablet

--- src/js/headerMenu.js
+++ src/js/headerMenu.js
@@ -25,32 +25,41 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 /*-------------home-link------------*/
-const homeModalLink = document.querySelector('#home-page-link-modal');
-const aboutModalLink = document.querySelector('#about-page-link-modal');
-const implModalLink = document.querySelector('#impl-page-link-modal');
-const updatesModalLink = document.querySelector('#updates-page-link-modal');
-const contactModalLink = document.querySelector('#contact-page-link-modal');
+document.addEventListener('DOMContentLoaded', () => {
+  const homeModalLink = document.querySelector('#home-page-link-modal');
+  const aboutModalLink = document.querySelector('#about-page-link-modal');
+  const implModalLink = document.querySelector('#impl-page-link-modal');
+  const updatesModalLink = document.querySelector('#updates-page-link-modal');
+  const contactModalLink = document.querySelector('#contact-page-link-modal');
+  const aboutLink = document.querySelector('#about-page-link');
+  const implLink = document.querySelector('#impl-page-link');
+  const updatesLink = document.querySelector('#updates-page-link');
+  const contaclLink = document.querySelector('#contact-page-link');
 
-const filename = window.location.pathname.split('/').pop();
-switch (filename) {
-  case 'index.html':
-    homeModalLink.style.display = 'none';
-    break;
-  case 'about.html':
-    aboutModalLink.classList.add('active-page');
-    break;
+  const filename = window.location.pathname.split('/').pop();
+  switch (filename) {
+    case 'index.html':
+      homeModalLink.style.display = 'none';
+      break;
+    case 'about.html':
+      aboutModalLink.classList.add('active-page');
+      aboutLink.classList.add('heading');
+      break;
 
-  case 'implementation.html':
-    implModalLink.classList.add('active-page');
-    break;
+    case 'implementation.html':
+      implModalLink.classList.add('active-page');
+      implLink.classList.add('heading');
+      break;
 
-  case 'updates.html':
-    updatesModalLink.classList.add('active-page');
-    break;
-  case 'contact.html':
-    contactModalLink.classList.add('active-page');
-    break;
+    case 'updates.html':
+      updatesModalLink.classList.add('active-page');
+      updatesLink.classList.add('heading');
+      break;
+    case 'contact.html':
+      contactModalLink.classList.add('active-page');
+      contaclLink.classList.add('heading');
+      break;
 
-  default:
-}
-console.log(window.location.pathname);
+    default:
+  }
+});
