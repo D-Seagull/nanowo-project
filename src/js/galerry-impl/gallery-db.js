bc686da2c7e0d9f4777d3c2a2a0a65621d776ce9
D-Seagull <136006992+Seagull-D@users.noreply.github.com>
2025-05-24 21:41:35 +0200

    fixed 2 x

--- src/js/galerry-impl/gallery-db.js
+++ src/js/galerry-impl/gallery-db.js
@@ -1,5 +1,5 @@
 const galleriesList = {
-  1:["././img/gallery-impl/gallery1/DSC03257-min.JPG","././img/gallery-impl/gallery1/DSC03360-min.JPG" ]
+  1:["./img/gallery-impl/gallery1/DSC03257-min.JPG","./img/gallery-impl/gallery1/DSC03360-min.JPG" ]
 }
 
 export default galleriesList;
