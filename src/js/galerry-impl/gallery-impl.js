edec556076798dfbf696803e8e8883abaa459587
D-Seagull <136006992+Seagull-D@users.noreply.github.com>
2025-05-24 23:42:09 +0200

    added data button cheking

--- src/js/galerry-impl/gallery-impl.js
+++ src/js/galerry-impl/gallery-impl.js
@@ -6,13 +6,25 @@ const implLinkBtn = document.querySelectorAll(".js-impl-gallery");
 implLinkBtn.forEach(btn => {
   btn.addEventListener("click", handleCreateGallery)
 
+
 });
-function handleCreateGallery(){
+function handleCreateGallery(evt){
+ const button = evt.currentTarget;
+const buttonData = button.dataset.implgallery;
+console.log(buttonData);
 for (const key in galleriesList){
+  console.log(key);
+if(key === buttonData){
 const gallery = galleriesList[key];
 console.log(gallery);
+galleryList.innerHTML = ""
+
 galleryList.insertAdjacentHTML("beforeend", createHtmlEl(gallery));
+}else {
+  galleryList.innerHTML = '<p>Zdjęcia tej realizacji już wkrótce!</p>'
 }
+}
+
 }
 
 function createHtmlEl(arr) {
