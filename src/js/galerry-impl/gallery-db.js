69d396a397cd308c326c366c5129bfc39c242c2f
D-Seagull <136006992+Seagull-D@users.noreply.github.com>
2025-05-24 22:46:28 +0200

    moved  gallery to pablic

--- src/js/galerry-impl/gallery-db.js
+++ src/js/galerry-impl/gallery-db.js
@@ -2,8 +2,8 @@ const base = import.meta.env.BASE_URL;
 
 const galleriesList = {
   1: [
-    `${base}img/gallery-impl/gallery1/DSC03257-min.JPG`,
-    `${base}img/gallery-impl/gallery1/DSC03360-min.JPG`,
+    `${base}gallery-impl/gallery1/DSC03257-min.JPG`,
+    `${base}gallery-impl/gallery1/DSC03360-min.JPG`,
   ],
 };
 
