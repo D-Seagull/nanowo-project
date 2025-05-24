656b33727273cacb625df0974ca5aacb55915098
D-Seagull <136006992+Seagull-D@users.noreply.github.com>
2025-05-24 22:19:59 +0200

    test 7

--- src/js/galerry-impl/gallery-impl.js
+++ src/js/galerry-impl/gallery-impl.js
@@ -3,37 +3,35 @@ import galleriesList from "./gallery-db";
 const galleryList = document.querySelector(".gallery");
 const implLinkBtn = document.querySelectorAll(".js-impl-gallery");
 
-implLinkBtn.forEach((btn) => {
-  btn.addEventListener("click", handleCreateGallery);
+implLinkBtn.forEach(btn => {
+  btn.addEventListener("click", handleCreateGallery)
+
 });
+function handleCreateGallery(){
+for (const key in galleriesList){
+const gallery = galleriesList[key];
+console.log(gallery);
+galleryList.insertAdjacentHTML("beforeend", createHtmlEl(gallery));
+}
+}
 
 function createHtmlEl(arr) {
   return arr
     .map(
-      (item) => `
-        <li class="gallery-item">
-          <a class="gallery-link" href="${item}">
-            <img class="gallery-image" src="${item}" alt="img" width="360" />
-          </a>
-        </li>
-      `
+      (item) =>
+        `<li class="gallery-item">
+	<a class="gallery-link" href="${item}">
+		<img
+			class="gallery-image"
+			src="${item}"
+			alt="img" width="360"
+			/>
+	</a>
+</li>
+`
     )
     .join("");
+
 }
 
-function handleCreateGallery(e) {
-  const btn = e.currentTarget;
-  const galleryIndex = btn.dataset.gallery; // наприклад data-gallery="1"
 
-  const gallery = galleriesList[galleryIndex];
-  if (!gallery) {
-    console.error(`Gallery with index ${galleryIndex} not found.`);
-    return;
-  }
-
-  // Очищення перед вставкою нової галереї (опціонально)
-  galleryList.innerHTML = "";
-
-  // Вставка
-  galleryList.insertAdjacentHTML("beforeend", createHtmlEl(gallery));
-}
