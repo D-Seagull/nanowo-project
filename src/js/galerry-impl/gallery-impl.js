e766312c9b40e80b47bf6f9ee994b053a6f848eb
D-Seagull <136006992+Seagull-D@users.noreply.github.com>
2025-05-25 19:09:35 +0200

    added js gallery

--- src/js/galerry-impl/gallery-impl.js
+++ src/js/galerry-impl/gallery-impl.js
@@ -1,49 +1,57 @@
-import galleriesList from "./gallery-db";
+import galleriesList from './gallery-db';
 
-const galleryList = document.querySelector(".gallery");
-const implLinkBtn = document.querySelectorAll(".js-impl-gallery");
+const galleryList = document.querySelector('.gallery');
+const implLinkBtn = document.querySelectorAll('.js-impl-gallery');
+const impProjectsPage = document.querySelector('#imp-projects');
+const impGalleryPage = document.querySelector('#implementation-gallery');
+const impBackBtn = document.querySelector('#imp-gallery-back');
+
+impBackBtn.addEventListener("click", () =>{
+  impProjectsPage.classList.toggle('hidden');
+  impGalleryPage.classList.toggle('hidden');
+})
 
 implLinkBtn.forEach(btn => {
-  btn.addEventListener("click", handleCreateGallery)
-
-
+  btn.addEventListener('click', handleCreateGallery);
 });
-function handleCreateGallery(evt){
- const button = evt.currentTarget;
-const buttonData = button.dataset.implgallery;
-console.log(buttonData);
-for (const key in galleriesList){
-  console.log(key);
-if(key === buttonData){
-const gallery = galleriesList[key];
-console.log(gallery);
-galleryList.innerHTML = ""
+function handleCreateGallery(evt) {
+  impProjectsPage.classList.toggle('hidden');
+  impGalleryPage.classList.toggle('hidden');
+  const button = evt.currentTarget;
+  const buttonData = button.dataset.implgallery;
+  console.log(buttonData);
+  for (const key in galleriesList) {
+    console.log(key);
+    if (key === buttonData) {
+      const gallery = galleriesList[key];
+      console.log(gallery);
+      galleryList.innerHTML = '';
 
-galleryList.insertAdjacentHTML("beforeend", createHtmlEl(gallery));
-}else {
-  galleryList.innerHTML = '<p>Zdjęcia tej realizacji już wkrótce!</p>'
-}
-}
-
+      galleryList.insertAdjacentHTML('beforeend', createHtmlEl(gallery));
+    } else {
+      galleryList.innerHTML = '<p> 🏗️ Zdjęcia tej realizacji już wkrótce!</p>';
+    }
+  }
 }
 
 function createHtmlEl(arr) {
   return arr
     .map(
-      (item) =>
-        `<li class="gallery-item">
-	<a class="gallery-link" href="${item}">
-		<img
-			class="gallery-image"
-			src="${item}"
-			alt="img" width="360"
-			/>
-	</a>
-</li>
+      item =>
+        ` <li class="gallery-item">
+    <a class="gallery-link" href="${item["1x"]}">
+      <img
+        class="gallery-image"
+        src="${item["1x"]}"
+        srcset="${item["1x"]} 1x, ${item["2x"]} 2x"
+        alt="img"
+        width="360"
+        loading="lazy"
+      />
+    </a>
+  </li>
 `
     )
-    .join("");
-
+    .join('');
 }
-
 
