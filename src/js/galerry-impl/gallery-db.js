ebd726069be233c2dd6b03c48dff9ecba73d4240
D-Seagull <136006992+Seagull-D@users.noreply.github.com>
2025-05-25 20:11:58 +0200

    gallery load more added

--- src/js/galerry-impl/gallery-db.js
+++ src/js/galerry-impl/gallery-db.js
@@ -37,118 +37,118 @@ const galleriesList = {
   '1x': `${base}gallery-impl/gallery1/DSC03349-min.JPG`,
   '2x': `${base}gallery-impl/gallery1-2x/DSC03349-min.JPG`,
 },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03360-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03360-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03361-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03361-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03367-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03367-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03374-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03374-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03380-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03380-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03388-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03388-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03395-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03395-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03398-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03398-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03402-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03402-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03408-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03408-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03414-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03414-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03416-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03416-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03418-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03418-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03421-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03421-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03426-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03426-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03436-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03436-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03440-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03440-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03444-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03444-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03451-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03451-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03456-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03456-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03459-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03459-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03463-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03463-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03471-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03471-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03472-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03472-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03479-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03479-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03482-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03482-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03491-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03491-min.JPG`,
-// },
-// {
-//   '1x': `${base}gallery-impl/gallery1/DSC03499-min.JPG`,
-//   '2x': `${base}gallery-impl/gallery1-2x/DSC03499-min.JPG`,
-// },
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03360-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03360-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03361-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03361-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03367-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03367-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03374-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03374-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03380-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03380-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03388-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03388-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03395-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03395-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03398-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03398-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03402-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03402-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03408-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03408-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03414-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03414-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03416-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03416-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03418-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03418-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03421-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03421-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03426-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03426-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03436-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03436-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03440-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03440-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03444-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03444-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03451-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03451-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03456-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03456-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03459-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03459-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03463-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03463-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03471-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03471-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03472-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03472-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03479-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03479-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03482-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03482-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03491-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03491-min.JPG`,
+},
+{
+  '1x': `${base}gallery-impl/gallery1/DSC03499-min.JPG`,
+  '2x': `${base}gallery-impl/gallery1-2x/DSC03499-min.JPG`,
+},
 
 
   ],
--- src/js/galerry-impl/gallery-impl.js
+++ src/js/galerry-impl/gallery-impl.js
@@ -2,56 +2,81 @@ import galleriesList from './gallery-db';
 
 const galleryList = document.querySelector('.gallery');
 const implLinkBtn = document.querySelectorAll('.js-impl-gallery');
-const impProjectsPage = document.querySelector('#imp-projects');
+const impProjectsPage = document.querySelector('.hidden-wrap');
 const impGalleryPage = document.querySelector('#implementation-gallery');
 const impBackBtn = document.querySelector('#imp-gallery-back');
+const loadMoreBtn = document.querySelector('#load-more');
 
-impBackBtn.addEventListener("click", () =>{
-  impProjectsPage.classList.toggle('hidden');
-  impGalleryPage.classList.toggle('hidden');
-})
+let currentGallery = [];
+let loadedCount = 0;
+const ITEMS_PER_PAGE = 6;
+
+impBackBtn.addEventListener('click', () => {
+  impProjectsPage.classList.remove('hidden');
+  impGalleryPage.classList.add('hidden');
+  galleryList.innerHTML = '';
+  currentGallery = [];
+  loadedCount = 0;
+  loadMoreBtn.classList.add('hidden');
+});
+
 
 implLinkBtn.forEach(btn => {
   btn.addEventListener('click', handleCreateGallery);
 });
-function handleCreateGallery(evt) {
-  impProjectsPage.classList.toggle('hidden');
-  impGalleryPage.classList.toggle('hidden');
-  const button = evt.currentTarget;
-  const buttonData = button.dataset.implgallery;
-  console.log(buttonData);
-  for (const key in galleriesList) {
-    console.log(key);
-    if (key === buttonData) {
-      const gallery = galleriesList[key];
-      console.log(gallery);
-      galleryList.innerHTML = '';
 
-      galleryList.insertAdjacentHTML('beforeend', createHtmlEl(gallery));
-    } else {
-      galleryList.innerHTML = '<p> 🏗️ Zdjęcia tej realizacji już wkrótce!</p>';
-    }
+function handleCreateGallery(evt) {
+  impProjectsPage.classList.add('hidden');
+  impGalleryPage.classList.remove('hidden');
+
+  const buttonData = evt.currentTarget.dataset.implgallery;
+  const gallery = galleriesList[buttonData];
+
+  galleryList.innerHTML = '';
+
+  if (gallery && gallery.length > 0) {
+    currentGallery = gallery;
+    loadedCount = 0;
+
+    renderNextImages();
+    loadMoreBtn.classList.remove('hidden');
+  } else {
+    galleryList.innerHTML = '<p> 🏗️ Zdjęcia tej realizacji już wkrótce!</p>';
+    loadMoreBtn.classList.add('hidden');
   }
 }
 
 function createHtmlEl(arr) {
   return arr
     .map(
-      item =>
-        ` <li class="gallery-item">
-    <a class="gallery-link" href="${item["1x"]}">
-      <img
-        class="gallery-image"
-        src="${item["1x"]}"
-        srcset="${item["1x"]} 1x, ${item["2x"]} 2x"
-        alt="img"
-        width="360"
-        loading="lazy"
-      />
-    </a>
-  </li>
-`
+      item => `
+        <li class="gallery-item">
+          <a class="gallery-link" href="${item['1x']}">
+            <img
+              class="gallery-image"
+              src="${item['1x']}"
+              srcset="${item['1x']} 1x, ${item['2x']} 2x"
+              alt="img"
+              loading="lazy"
+            />
+          </a>
+        </li>
+      `
     )
     .join('');
 }
 
+function renderNextImages() {
+  const nextItems = currentGallery.slice(
+    loadedCount,
+    loadedCount + ITEMS_PER_PAGE
+  );
+  galleryList.insertAdjacentHTML('beforeend', createHtmlEl(nextItems));
+  loadedCount += ITEMS_PER_PAGE;
+
+  if (loadedCount >= currentGallery.length) {
+    loadMoreBtn.classList.add('hidden');
+  }
+}
+
+loadMoreBtn.addEventListener('click', renderNextImages);
