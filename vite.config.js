4d0cecdd47bbd2b1820d902bd49512e8856cc76b
D-Seagull <dchaika.work@gmail.com>
2025-07-11 19:21:45 +0200

    changed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,45 @@
 import { defineConfig } from 'vite';
+import path from 'path';
 import { glob } from 'glob';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
-import path from 'path';
 
-// Отримуємо всі HTML-файли у папці src
-const inputs = glob.sync('src/*.html');
+// Отримуємо всі HTML-файли в src/
+const inputs = glob.sync('./src/*.html').map(file => path.resolve(file));
 
 export default defineConfig(({ command }) => ({
-  base: './', // Відносні шляхи, щоб в продакшені було коректно
+  base:   './',
+  define: {
+    [command === 'serve' ? 'global' : '_global']: {},
+  },
   root: 'src',
   build: {
-    outDir: path.resolve(__dirname, 'dist'), // Збираємо у папку dist на рівні з src
-    emptyOutDir: true,
     sourcemap: true,
     rollupOptions: {
-      input: inputs.length ? inputs : 'src/index.html',
+      input: inputs.length ? inputs : path.resolve('src/index.html'), // Якщо немає файлів, використовуємо index.html
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
             return 'vendor';
           }
         },
-        entryFileNames: '[name].js',
-        assetFileNames: 'assets/[name]-[hash][extname]',
+        entryFileNames: chunkInfo => {
+          if (chunkInfo.name === 'commonHelpers') {
+            return 'commonHelpers.js';
+          }
+          return '[name].js';
+        },
+        assetFileNames: assetInfo => {
+          if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+            return '[name].[ext]';
+          }
+          return 'assets/[name]-[hash][extname]';
+        },
       },
     },
+    outDir: '../dist',
+    emptyOutDir: true,
   },
   optimizeDeps: {
     include: [
@@ -37,7 +50,7 @@ export default defineConfig(({ command }) => ({
   },
   plugins: [
     injectHTML(),
-    FullReload(['src/**/*.html']),
+    FullReload(['./src/**/**.html']),
     SortCss({
       sort: 'mobile-first',
     }),
